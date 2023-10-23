e89f5ea90951e41bbac8c8ea1602cc73a9704676
AZKI Habibi <azkalfrisi212@gmail.com>
2023-10-23 14:47:41 +0700

    database_w10

---
+++ config/db.config.js
@@ -0,0 +1,11 @@
+
+const dbConfig = {
+    HOST: 'localhost',
+    USER: 'root',
+    PASSWORD: 'azkiaic34',
+    DATABASE: 'modul10',
+    DIALECT: 'mysql',
+    PORT: 3306
+}
+
+module.exports = dbConfig;
